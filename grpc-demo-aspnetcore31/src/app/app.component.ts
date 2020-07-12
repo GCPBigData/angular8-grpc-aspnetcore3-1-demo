c49f50b5699c7d130896b479bbb0f353c667c18b
Jose RF Junior <web2ajax@gmail.com>
2020-07-12 12:01:25 -0400

    update angualar 10

--- grpc-demo-aspnetcore31/src/app/app.component.ts
+++ grpc-demo-aspnetcore31/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {grpc} from '@improbable-eng/grpc-web';
 import {CountryService} from './generated/country_pb_service';
-import {EmptyRequest, CountriesReply} from './generated/country_pb';
+import {EmptyRequest, CountriesReply, CountryCreateRequest} from './generated/country_pb';
 import { CountryModel } from './models/countryModel';
 
 @Component({
@@ -16,10 +16,13 @@ export class AppComponent implements OnInit {
 
   public ngOnInit() {
 
-    const getCountryRequest = new EmptyRequest();
+    const getCountryRequestAll = new EmptyRequest();
+    const getCountryRequestId = new EmptyRequest();
+    const setCountryCreateRequest = new EmptyRequest();
+    const getCountrySearchRequest = new EmptyRequest();
 
     grpc.unary(CountryService.GetAll, {
-      request: getCountryRequest,
+      request: getCountryRequestAll,
       /*https://grpcwebdemo.azurewebsites.net (Windows App Service)*/
       host: 'https://demogrpcweblinux.azurewebsites.net',
       onEnd: res => {
@@ -34,5 +37,57 @@ export class AppComponent implements OnInit {
         }
       }
     });
+
+    grpc.unary(CountryService.GetAll, {
+      request: getCountryRequestId,
+      /*https://grpcwebdemo.azurewebsites.net (Windows App Service)*/
+      host: 'https://demogrpcweblinux.azurewebsites.net',
+      onEnd: res => {
+        const { status, statusMessage, headers, message, trailers } = res;
+        if (status === grpc.Code.OK && message) {
+          const result = message.toObject() as CountriesReply.AsObject;
+          this.countries = result.countriesList.map(country =>
+            ({
+              name: country.name,
+              description: country.description
+            }) as CountryModel);
+        }
+      }
+    });
+
+    grpc.unary(CountryService.GetAll, {
+      request: getCountrySearchRequest,
+      /*https://grpcwebdemo.azurewebsites.net (Windows App Service)*/
+      host: 'https://demogrpcweblinux.azurewebsites.net',
+      onEnd: res => {
+        const { status, statusMessage, headers, message, trailers } = res;
+        if (status === grpc.Code.OK && message) {
+          const result = message.toObject() as CountriesReply.AsObject;
+          this.countries = result.countriesList.map(country =>
+            ({
+              name: country.name,
+              description: country.description
+            }) as CountryModel);
+        }
+      }
+    });
+
+    grpc.unary(CountryService.GetAll, {
+      request: setCountryCreateRequest,
+      /*https://grpcwebdemo.azurewebsites.net (Windows App Service)*/
+      host: 'https://demogrpcweblinux.azurewebsites.net',
+      onEnd: res => {
+        const { status, statusMessage, headers, message, trailers } = res;
+        if (status === grpc.Code.OK && message) {
+          const result = message.toObject() as CountriesReply.AsObject;
+          this.countries = result.countriesList.map(country =>
+            ({
+              name: country.name,
+              description: country.description
+            }) as CountryModel);
+        }
+      }
+    });
+
   }
 }
